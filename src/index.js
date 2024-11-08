f49115b7246c3b3c7293c2ac2d8fba6985f56988
Sara Garcia <saragpwork95@gmail.com>
2024-11-08 18:33:21 +0000

    iterations done

--- src/index.js
+++ src/index.js
@@ -5,9 +5,8 @@ let timer = null; // Variable to store the interval
 
 
 // ITERATION 1: Add event listener to the start button
-
-// Your code goes here ...
-
+const startButton = document.getElementById("start-btn");
+startButton.addEventListener("click", () => startCountdown());
 
 
 
@@ -15,8 +14,38 @@ let timer = null; // Variable to store the interval
 function startCountdown() {
   console.log("startCountdown called!");
 
+  const timeDisplay = document.getElementById("time");
 
-  // Your code goes here ...
+  remainingTime = DURATION;
+  startButton.disable = true;
+
+
+  const countdownInterval = setInterval(() => {
+    remainingTime--
+    timeDisplay.textContent = remainingTime;
+
+
+    // BONUS 5
+    if (remainingTime >= 9) {
+      showToast("⏰ Final countdown! ⏰");
+
+    } else if (remainingTime === 5) {
+      showToast("Start the engines! 💥");
+
+    } else if (remainingTime <= 0) {
+      showToast("Lift off! 🚀");
+      clearInterval(countdownInterval);
+      startButton.disable = false;
+    }
+
+
+    /*if (remainingTime <= 0) {
+      clearInterval(countdownInterval);
+      startButton.disable = false;
+      showToast();
+    }*/
+
+  }, 1000);
 }
 
 
@@ -26,13 +55,26 @@ function startCountdown() {
 function showToast(message) {
   console.log("showToast called!");
 
-  // Your code goes here ...
+  const toastCard = document.getElementById("toast");
+  toastCard.classList.add("show")
+
+  const toastTimeOut = setTimeout(() => {
+    toastCard.classList.remove("show");
+  }, 3000)
+
+ 
+
+  // BONUS 4: Add click event to the close button
+  const closeToast = document.getElementById("close-toast")
+ 
+  closeToast.addEventListener("click", () => {
+    clearTimeout(toastTimeOut);
+    toastCard.classList.remove("show")
+  })
 
 
-
-
-  // BONUS: ITERATION 4: TOAST CLOSE BUTTON
-
-  // Your code goes here ...
-
+  //BONUS 5
+  const toastMessage = document.getElementById("toast-message"); 
+  toastMessage.textContent = message;
 }
+
